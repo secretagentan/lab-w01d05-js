81748b3e71de285f50812c6a4d5c8f77a6cadc06
an nguyen <hello.an.ng@gmail.com>
2016-12-05 06:20:04 -0800

    saves work

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ var handleAddTodo = function(event) {
     li.appendChild(span);
 
     // adds text to span element
-    span.appendChild(document.createTextNode("x"));
+    span.appendChild(document.createTextNode("x "));
 
     // set class of todo to each list item
     li.setAttribute("class", "todo");
@@ -56,12 +56,12 @@ input.addEventListener("keypress", handleAddTodo);
 // II. REMOVES TO-DO ITEM FROM LIST
 // ================================================
 
-var remove = document.querySelector(".remove");
-remove.addEventListener("click", handleRemove);
+// var removeLi = document.querySelector(".remove");
+// removeLi.addEventListener("click", handleRemove);
 
-function handleRemove(event) {
-  console.log("clicked", event);
-}
+// function handleRemove(event) {
+//   console.log("clicked", event);
+// }
 
 // var handleRemove = function(event) {
 //   console.log(event, event.target, event.currentTarget);
@@ -79,12 +79,42 @@ function handleRemove(event) {
 
 // });
 
+// var removeLi = function(event) {
+//   var remove = document.querySelector(".remove");
+//   if (remove.)
+// }
 
 
+// // 1. Query the DOM or a node that you want to put an event on
+// var removeLi = document.querySelector(".remove");
 
+// // 2. Write a function to handle that event
+// var handleRemove = function(event) {
 
+// // 3. Give the function a parameter for that event object
 
+// // 4. If you want to do something to what you clicked on, use event.target
+//   // 4a. Input tags have .value (event.target.value) and .keyCode
+//   // 4b. Other tags have textContent
+//   var ul = document.querySelector(".todos");
+//   var li = document.querySelector(".todo");
 
+//   event.target.remove();
+//   removeLi.remove();
+// // 5. Pick the event string you want to trigger it with
+// // 6. Add the event listener to the queried node
+// };
+// // 7. Give the event listener the event string and the callback function
+
+// var removeLi = document.querySelector("span");
+
+// var handleRemove = function(info) {
+//   console.log("you clicked me", info);
+//   console.log("target", info.target);
+//   console.log("currentTarget", info.currentTarget);
+// };
+
+// removeLi.addEventListener("click", handleRemove);
 
 
 
@@ -93,7 +123,15 @@ function handleRemove(event) {
 // III. COMPLETES TO-DO ITEM ON LIST
 // ================================================
 
+// var todo = document.querySelectorAll(".todo");
 
+// var completeLi = function(event) {
+//   if (event.target.onClick) {
+//     event.target.toggleClass("complete");
+//   }
+// };
+
+// todo.addEventListener("click", completeLi);
 
 
 
